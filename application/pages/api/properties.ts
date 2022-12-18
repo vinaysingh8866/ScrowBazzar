4843e133467c2fb8d9321b6d2e178bfcbc58b686
Vinay Singh <vinaysingh8866@gmail.com>
2022-12-17 20:57:55 -0500

    removed redundant web application

--- application/pages/api/properties.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { connectToDatabase } from "../../util/mangodb";
-import { getSession } from "next-auth/react";
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { db } = await connectToDatabase();
-    res.json({ name: 'John Doe' })
-}
